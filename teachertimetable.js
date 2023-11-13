e3d65d4fd622dc355dea7a54b9bcd98407697f40
Simarjeet Singh <124123643+AlphaSimar@users.noreply.github.com>
2023-11-14 03:47:43 +0530

    Add files via upload

---
+++ teachertimetable.js
@@ -0,0 +1,134 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '5220',
+        subject: 'CSAI-I',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '5221',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-12 AM',
+        roomNumber: '5220',
+        subject: 'CSAI-I ',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-02 PM',
+        roomNumber: '5115',
+        subject: 'MAC-I',
+        type: 'LAB'
+    },
+]
+
+const Wednesday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '5220',
+        subject: 'CSAI-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '5220',
+        subject: 'CSAI-I ',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-02 PM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Tutorial'
+    },
+]
+
+const Thursday = [
+    {   
+        time: '02-03 PM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-06 PM',
+        roomNumber: '5220',
+        subject: 'CSAI-I ',
+        type: 'Tutorial'
+    },
+
+]
+
+const Friday = [
+    {   
+        time: '02-03 PM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '5222',
+        subject: 'MAC-I',
+        type: 'Tutorial'
+    },
+    {   
+        time: '04-06 PM',
+        roomNumber: '5220',
+        subject: 'CSAI-I',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
---
+++ timeTable.js
@@ -0,0 +1,134 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: '5222',
+        subject: 'DBMS(CMCSC05)',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '5222',
+        subject: 'Mathematical Statistics (CMMTC08)',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: '5222',
+        subject: 'Engineering Mathematics (CMMTC04)',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '5222',
+        subject: 'Mathematical Statistics (CMMTC08)',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '5222',
+        subject: 'CAO(CMCSC07)',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-12 AM',
+        roomNumber: '5115',
+        subject: 'DBMS ',
+        type: 'LAB'
+    },
+    {   
+        time: '12-02 PM',
+        roomNumber: '5222',
+        subject: 'Mathematical Statistics (CMMTC08)',
+        type: 'Tutorial'
+    },
+]
+
+const Wednesday =[
+    {   
+        time: '08-09 AM',
+        roomNumber: '5222',
+        subject: 'DAA ',
+        type: 'Lecture'
+    },
+    {   
+        time: '09-10 AM',
+        roomNumber: '5222',
+        subject: 'Engineering Mathematics (CMMTC04)',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: '5222',
+        subject: 'DBMS ',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-02 PM',
+        roomNumber: '5222',
+        subject: 'Engineering Mathematics (CMMTC04)',
+        type: 'Tutorial'
+    },
+]
+
+const Thursday = [
+    {   
+        time: '02-03 PM',
+        roomNumber: '5222',
+        subject: 'DAA ',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '5222',
+        subject: 'Engineering Mathematics (CMMTC04)',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-06 PM',
+        roomNumber: '5115',
+        subject: 'DAA ',
+        type: 'LAB'
+    },
+
+]
+
+const Friday = [
+    {   
+        time: '02-03 PM',
+        roomNumber: '5222',
+        subject: 'CAO',
+        type: 'Lecture'
+    },
+    {   
+        time: '03-04 PM',
+        roomNumber: '5222',
+        subject: 'Mathematical Statistics (CMMTC08)',
+        type: 'Lecture'
+    },
+    {   
+        time: '04-06 PM',
+        roomNumber: '5222',
+        subject: 'CAO',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
